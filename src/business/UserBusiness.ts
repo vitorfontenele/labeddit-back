f83bb3c0e02b2feaa37e34a496ad8202dfb9f31f
vitor-fontenele <vitor_fontenele@hotmail.com>
2023-03-09 19:50:04 -0300

    Add UserBusiness

---
+++ src/business/UserBusiness.ts
@@ -0,0 +1,4 @@
+import { UserDatabase } from "../database/UserDatabase";
+import { BadRequestError } from "../errors/BadRequestError";
+import { NotFoundError } from "../errors/NotFoundError";
+import { User } from "../models/User";
